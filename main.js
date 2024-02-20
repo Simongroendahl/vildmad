9d5aef861d5e95a4587a6db8445ab3ddee6a0a91
Simon <sila0002@stud.kea.dk>
2024-02-20 21:00:17 +0100

    Opsætter data i JavaScript

--- main.js
+++ main.js
@@ -3,13 +3,15 @@
 
 const data = "";
 
+const card = document.querySelector("forageCard");
+
 window.addEventListener("load", sidenVises);
 
 function sidenVises() {
   console.log("sidenVises");
 }
 
-fetch("https://ytcuyyezqmwdqdpjhotr.supabase.co/rest/v1/VildMad", {
+fetch("https://ytcuyyezqmwdqdpjhotr.supabase.co/rest/v1/vildmad", {
   method: "GET",
   headers: {
     apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0Y3V5eWV6cW13ZHFkcGpob3RyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc5NDY4OTcsImV4cCI6MjAyMzUyMjg5N30.F5qT6xHF4HXJc6yuBAtGegp4xfqgo5aM29_Yg2767LA",
@@ -19,5 +21,20 @@ fetch("https://ytcuyyezqmwdqdpjhotr.supabase.co/rest/v1/VildMad", {
   .then(showData);
 
 function showData(items) {
-  console.log(items);
+  console.table(items);
+  //Her looper vi
+  items.forEach(showIngredient);
+}
+
+function showIngredient(ingredient) {
+  const temp = document.querySelector("#forageCardTemplate").content;
+  const copy = temp.cloneNode(true);
+  copy.querySelector("h1").textContent = ingredient.title;
+  copy.querySelector("p.find_p").textContent = ingredient.wheretofind;
+  copy.querySelector("p.spot_p").textContent = ingredient.howtospot;
+  copy.querySelector("p.pick_p").textContent = ingredient.howtopick;
+  copy.querySelector("p.reading_p").textContent = ingredient.continuedreading;
+  copy.querySelector("img.profile_img").src = ingredient.image;
+  const parent = document.querySelector("#ingredientsGrid");
+  parent.appendChild(copy);
 }
